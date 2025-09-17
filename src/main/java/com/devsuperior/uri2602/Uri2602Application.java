18ab5776d4f6fa74b19531e308f2448bcc5de335
Alexandre Matos <matosalexandre795@gmail.com>
2025-09-17 14:11:18 -0300

    New DTO - CustomerMinDTO

--- src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,14 @@
 package com.devsuperior.uri2602;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
 import com.devsuperior.uri2602.projections.CustomerMinProjection;
 import com.devsuperior.uri2602.repositories.CustomerRepository;
 
@@ -23,8 +25,9 @@ public class Uri2602Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		List<CustomerMinProjection> list = customerRepository.search1("RS");
-		for (CustomerMinProjection obj : list) {
-			System.out.println(obj.getName());
+		List<CustomerMinDTO> dtoList = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
+		for (CustomerMinDTO obj : dtoList) {
+			System.out.println(obj);
 		}
 	}
 }
